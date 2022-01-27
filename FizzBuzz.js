9b6a08bf212885abff5b0ed0bde75aa6833a3ddb
Elijah Palmer <96744423+ElijahMPalmer@users.noreply.github.com>
2022-01-26 19:59:39 -0800

    Add files via upload

---
+++ FizzBuzz.js
@@ -0,0 +1,16 @@
+var fizzBuzz = function(n) {
+    var answer = [];
+    for (var i = 1; i <= n; i++) {
+        if (i % 15 === 0) {
+            answer.push('FizzBuzz');
+        } else if (i % 5 === 0) {
+            answer.push('Buzz');
+        } else if (i % 3 === 0) {
+            answer.push('Fizz');
+        } else {
+            answer.push(i.toString());
+        }
+    }
+    return answer;
+}
+console.log(fizzBuzz(15));
